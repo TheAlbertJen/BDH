22c4a6ce8bdd46126a46abdc57c5352032f13449
TheAlbertJen <thealbertjen@gmail.com>
2015-02-16 00:30:47 -0800

    Initial setup for BDH game

---
+++ Assets/Scripts/MapGeneration/RandomWorldGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RandomWorldGenerator : MonoBehaviour
+{
+
+    public int mapSeed;
+    public int mapHeight;
+    public int mapWidth;
+
+    private int[] mapValues;
+
+    // height map values: ocean, coast, land, hills, mountains
+    private int[] heightMap;
+    
+
+    void Start ()
+    {
+        Random.seed = mapSeed;
+        mapValues = new int[mapWidth * mapHeight];
+        heightMap = new int[mapWidth * mapHeight];
+
+        // default world to ocean
+        for (int i = 0; i < heightMap.Length; i++)
+            heightMap[i] = -1;
+	}
+
+    void Update()
+    {
+	
+	}
+
+    void CreateMap()
+    {
+        // create seed islands
+
+        // 
+    }
+
+
+    void WriteToFile()
+    {
+        // save the generated map to a file
+    }
+}
