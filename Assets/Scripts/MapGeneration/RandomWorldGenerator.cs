686034deb1c1ed394239eb0deda4534b94f33f67
TheAlbertJen <thealbertjen@gmail.com>
2015-02-25 23:32:23 -0800

    added font files

--- Assets/Scripts/MapGeneration/RandomWorldGenerator.cs
+++ Assets/Scripts/MapGeneration/RandomWorldGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Delaunay;
+using Delaunay.Geo;
 
 
 public class RandomWorldGenerator : MonoBehaviour
@@ -9,35 +11,49 @@ public class RandomWorldGenerator : MonoBehaviour
     public int mapSeed;
     public int mapHeight;
     public int mapWidth;
+    public int numSeeds = 4096;
 
     private int[] mapValues;
 
-    // height map values: ocean, coast, land, hills, mountains
-    private int[] heightMap;
-    
+    // seed points to start the map generation
+    private List<Vector2> seedPoints;
 
-    void Start ()
+    // status message for displaying to the screen
+    private string statusMessage;
+
+    void Start()
     {
-        Random.seed = mapSeed;
+        UnityEngine.Random.seed = mapSeed;
         mapValues = new int[mapWidth * mapHeight];
-        heightMap = new int[mapWidth * mapHeight];
 
-        // default world to ocean
-        for (int i = 0; i < heightMap.Length; i++)
-            heightMap[i] = -1;
-	}
+        seedPoints = new List<Vector2>(numSeeds);
+
+        statusMessage = "Creating a new world based on seed...";
+    }
 
     void Update()
     {
-	
-	}
+
+    }
+
 
     void CreateMap()
     {
-        // create seed islands
+        // generate random points
+        for (int i = 0; i < seedPoints.Count; i++)
+        {
+            int rX = UnityEngine.Random.Range(0, mapWidth);
+            int rY = UnityEngine.Random.Range(0, mapHeight);
 
-        // 
+            seedPoints[i] = new Vector2(rX, rY);
+        }
+
+        // generate voronoi using fortune's algorithm
+
     }
+
+
+
 
 
     void WriteToFile()
